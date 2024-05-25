192ca666737d83c35df0caeadd3007d374255739
Artem Kiyashko <artem86@hotmail.co.uk>
2024-05-25 18:52:19 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import ToggleOnIcon from '@mui/icons-material/ToggleOn';
+import ToggleOffIcon from '@mui/icons-material/ToggleOff';
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
 
 function App() {
+  const [clicked, setClicked] = React.useState(false);
+  const handleIconClick = async (id) => {
+    await fetch("https://faberkutlampserviceneprivate.azurewebsites.net/api/toggle?code=j3FIHPQlN7Hj4RREOVcoJVN_OoGAV19ODfG7MQr8liB8AzFuYydYVQ%3D%3D")
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+    setClicked(!clicked);
+  }
+
+  React.useEffect(() => {
+    window.Telegram.WebApp.ready();
+    window.Telegram.WebApp.expand();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <IconButton onClick={handleIconClick}>
+        {clicked ? <ToggleOnIcon color='primary' sx={{ width: '100%', height: '100%' }} /> : <ToggleOffIcon color='primary' sx={{ width: '100%', height: '100%' }} />}
+      </IconButton>
+    </Box>
   );
 }
 
