2a0bb365aecd4e2de6b4ee9923cddc4b0158531c
Artem Kiyashko <artem86@hotmail.co.uk>
2024-06-07 14:29:13 +0200

    Add signalR support

--- src/App.js
+++ src/App.js
@@ -1,34 +1,97 @@
-import ToggleOnIcon from '@mui/icons-material/ToggleOn';
-import ToggleOffIcon from '@mui/icons-material/ToggleOff';
 import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
 import BgImage from './img/background3.jpeg'
 import BgImageDark from './img/background3-lightsoff2.jpeg'
+import * as signalR from "@microsoft/signalr";
+import Switch from '@mui/material/Switch';
+import { styled } from '@mui/material/styles';
+
+const apiBaseUrl = 'https://amberkutweprivate.azure-api.net';
+const signalrendpoint = 'signalr';
+const lampSetStateEndpoint = 'berkutlamp/setstate';
+const lampGetStateEndpoint = 'berkutlamp/getstate';
+
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+  width: 124,
+  height: 68,
+  padding: 14,
+  '& .MuiSwitch-switchBase': {
+    margin: 2,
+    padding: 0,
+    transform: 'translateX(6px)',
+    '&.Mui-checked': {
+      color: '#fff',
+      transform: 'translateX(44px)',
+      '& .MuiSwitch-thumb:before': {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          '#fff',
+        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+      },
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+    width: 64,
+    height: 64,
+    '&::before': {
+      content: "''",
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      left: 0,
+      top: 0,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+        '#fff',
+      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+    },
+  },
+  '& .MuiSwitch-track': {
+    opacity: 1,
+    backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+    borderRadius: 40 / 2,
+  },
+}));
 
 function App() {
-  const [clicked, setClicked] = React.useState(false);
-  const [isLoading, setLoading] = React.useState(false);
-  const handleIconClick = async (id) => {
-    if (!isLoading) {
-      setLoading(true);
-      await fetch("https://amberkutweprivate.azure-api.net/berkutlamp/toggle")
-        .then(response => {
-          if (response.ok)
-            setClicked(!clicked);
-        })
-        .catch(error => {
-          console.error('There was an error!', error);
-        })
-        .finally(() => {
-          setLoading(false);
+  const [lampStatus, setLampStatus] = React.useState(false);
+  const handleIconClick = async (event) => {
+    fetch(`${apiBaseUrl}/${lampSetStateEndpoint}/${event.target.checked}`);
+    //setLampStatus(event.target.checked);
+  }
+
+  const loadLampStatus = async () => {
+    await fetch(`${apiBaseUrl}/${lampGetStateEndpoint}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((lampState) =>{
+        setLampStatus(lampState.state);
+      })
+      .finally(() => {
+        const connection = new signalR.HubConnectionBuilder()
+          .withUrl(`${apiBaseUrl}/${signalrendpoint}`)
+          .configureLogging(signalR.LogLevel.Information)
+          .build();
+        connection.on('lampstatuschanged', (lampStatus) => {
+          setLampStatus(lampStatus.Status);
         });
-    }
+
+        connection.start()
+          .catch(console.error);
+      });
   }
 
   React.useEffect(() => {
     window.Telegram.WebApp.ready();
     window.Telegram.WebApp.expand();
+
+    loadLampStatus();
   });
 
   return (
@@ -37,12 +100,11 @@ function App() {
       justifyContent: 'center',
       alignItems: 'center',
       height: '100vh',
-      backgroundImage: `${clicked ? `url(${BgImage})` : `url(${BgImageDark})`}`,
-      backgroundSize: 'contain'}}>
-      <IconButton onClick={handleIconClick}>
-        {clicked ? <ToggleOnIcon color='primary' sx={{ width: '100%', height: '100%' }} /> : <ToggleOffIcon color='primary' sx={{ width: '100%', height: '100%' }} />}
-      </IconButton>
-      <Box id='bgPreLoader' sx={{display: 'hidden', backgroundImage: `url(${BgImage})`}}></Box>
+      backgroundImage: `${lampStatus ? `url(${BgImage})` : `url(${BgImageDark})`}`,
+      backgroundSize: 'contain'
+    }}>
+      <MaterialUISwitch checked={lampStatus} onChange={handleIconClick} />
+      <Box id='bgPreLoader' sx={{ display: 'hidden', backgroundImage: `url(${BgImage})` }}></Box>
     </Box>
   );
 }
